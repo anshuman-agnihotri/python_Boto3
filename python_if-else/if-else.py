8759bcf78436118c1036fbb134d14c8ef184008d
anshuman agnihotri <anshuman.agnihotri@benthonlabs.com>
2025-01-06 01:21:26 +0530

    added if-else logic

--- python_if-else/if-else.py
+++ python_if-else/if-else.py
@@ -82,3 +82,68 @@ try:
         print("no discount is applied.")
 except ValueError:
     print("Invalid input! please enter valid ammount.")
+
+
+#  Write a program that checks if a year is a leap year. (Hint: A year is a leap year if it is divisible by 4, but not by 100, unless it is divisible by 400.)
+year = input ("Please enter the year. : ")
+try:
+    year = int (year)
+    if year%4 == 0 and year % 100 != 0 :
+        print ("this is leap year")
+    else:
+        ("this is not leap year.")
+except ValueError:
+    print("Invalid input! Please enter the numeric years")
+
+# Create a program that asks the user for three numbers and prints the smallest number.
+num1 = float(input("Please enter the first number. : "))
+num2 = float(input("Please enter the second value. : "))
+num3 = float(input("Please enter the third value. : "))
+small_number = min(num1, num2, num3)
+print(f"This is the small number: {small_number}")
+
+
+#  Write a program that takes an exam score as input and assigns grades as follows:
+# 90-100: A
+# 80-89: B
+# 70-79: C
+# 60-69: D
+# Below 60: F
+
+marks = int(input("Please enter the marks : "))
+if marks >= 90:
+    print("grade is A")
+elif marks >= 80:
+    print("Grade is B")
+elif marks >= 70:
+    print("Grade is C")
+elif marks >= 60:
+    print("Grade is D")
+else:
+    print("you are failed.")
+
+
+#  Write a program to simulate a login system:
+# Ask for a username and password.
+# If the username is admin and the password is 1234, print "Login successful."
+# Otherwise, print "Invalid credentials."
+
+username = input("Please enter the user name : ")
+password = int(input("Please enter the password : "))
+if username == 'admin' and password == 1234:
+    print("Login Successful")
+else:
+    print("Invalid credentials")
+
+# Create a program to check if a number is divisible by both 3 and 5.
+# Fun & Real-Life Scenarios
+
+number = input ("Please enter the number : ")
+try:
+    number = float(number)
+    if number % 3 == 0  and number % 5 == 0:
+        print("Number is divisible by 3 and 5")
+    else:
+        ("Number is not divisible by 3 and 5")
+except ValueError:
+    print("Invalid input! Please enter the numeric digits")
